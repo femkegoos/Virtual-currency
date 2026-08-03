aafc333dbf0ab0e92869e72685866887e86c2367
femkegoos <femke.goos.01@gmail.com>
2026-08-03 16:01:20 +0200

    chore: initialize project repository

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/lxq3ltx.css">
+    <title>To-Do App</title>
+</head> 
+
+<body>
+
+</body>
+
+
+</html>
