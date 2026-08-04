106cd760e3616b5a8bef3df0f0d4c677a3d844c6
femkegoos <femke.goos.01@gmail.com>
2026-08-04 18:30:28 +0200

    fix: link with the style file

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://use.typekit.net/lxq3ltx.css">
+<link rel="stylesheet" href="https://use.typekit.net/tkk7yuy.css">
     <title>XD Currency</title>
 </head> 
 <nav class="bottom-nav">
