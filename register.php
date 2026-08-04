c707083b3d50bf26e0f04f29ee82ff959792c9b2
femkegoos <femke.goos.01@gmail.com>
2026-08-04 18:16:30 +0200

    feat: add a register form

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registreren</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
+
+        <input type="submit" value="Registreer">
+    
+</body>
+</html>
