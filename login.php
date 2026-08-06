1551859907dd2887e9c95a8943b30d39e1a4eccd
femkegoos <femke.goos.01@gmail.com>
2026-08-06 17:35:56 +0200

    fix: error message on the next line

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ if (!empty($_POST)) {
             $_SESSION['id'] = $result['id'];
             exit;
         } else {
-            $error = "Ongeldige inloggegevens!" . nl2br(" Je email of wachtwoord is incorrect.");
+            $error = "Ongeldige inloggegevens!<br>Je email of wachtwoord is incorrect.";
         }
     } catch (Exception $e) {
         $error = $e->getMessage();
