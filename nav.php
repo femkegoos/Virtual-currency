62a97e4c1a407d1051dea418e90d86fa2858348d
femkegoos <femke.goos.01@gmail.com>
2026-08-08 15:14:43 +0200

    fix: navigation profile page

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
   <nav class="bottom-nav">
     <div class="nav-container">
     <a href="index.php"><img class="logo" alt="this is the logo of the XD Currency app" src="img/XD_currency_logo.png"></a>
-    <a href="register.php"><img class="profile" alt="this the icon of the profile page" src="img/XD_currency_icons_profile.png"></a>
+    <a href="profile.php"><img class="profile" alt="this the icon of the profile page" src="img/XD_currency_icons_profile.png"></a>
     <a href="transfer.php"><img class="transfer" alt="this the icon of the transfer page" src="img/XD_currency_icons_transfer.png"></a>
     <a href="list.php"><img class="list" alt="this the icon of the list page" src="img/XD_currency_icons_list.png"></a>
 </nav>
