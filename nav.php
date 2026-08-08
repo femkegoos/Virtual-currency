3e57c227183ed53ba646f47058f9736c415ffb81
femkegoos <femke.goos.01@gmail.com>
2026-08-08 19:45:52 +0200

    fix: font size of the saldo in the topo nav

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <header class="top-nav">
     <div class="top-nav-container">
-   <p class="saldo">Saldo: <?php echo htmlspecialchars($_SESSION['balance']); ?> XD</p>
+   <p class="saldo"><?php echo htmlspecialchars($_SESSION['balance']); ?> XD</p>
    <a href="logout.php"><img class="logout" alt="this the icon of the logout page" src="img/XD_currency_icons_logout.png"></a>
     </div>
 </header>
