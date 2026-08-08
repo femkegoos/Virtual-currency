78716ffc669d3edff4c90c688b3d4a33c335a177
femkegoos <femke.goos.01@gmail.com>
2026-08-08 15:07:16 +0200

    fix: put navigation in 1 document and include in all files

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once(__DIR__ . 'nav.php');
 ?>
 
 <!DOCTYPE html>
@@ -14,13 +14,7 @@
 
 
 <body>
-<nav class="bottom-nav">
-    <div class="nav-container">
-    <a href="index.php"><img class="logo" alt="this is the logo of the XD Currency app" src="img/XD_currency_logo.png"></a>
-    <a href="register.php"><img class="profile" alt="this the icon of the profile page" src="img/XD_currency_icons_profile.png"></a>
-    <a href="transfer.php"><img class="transfer" alt="this the icon of the transfer page" src="img/XD_currency_icons_transfer.png"></a>
-    <a href="list.php"><img class="list" alt="this the icon of the list page" src="img/XD_currency_icons_list.png"></a>
-</nav>
+
 
 </body>
 
--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 session_start();
 include_once(__DIR__ . '/classes/Db.php');
 include_once(__DIR__ . '/classes/User.php');
+include_once(__DIR__ . 'nav.php');
 
 if(!empty($_POST)) {
     try{
@@ -38,13 +39,7 @@ if(!empty($_POST)) {
 </head>
 
 <body>
-    <nav class="bottom-nav">
-    <div class="nav-container">
-    <a href="index.php"><img class="logo" alt="this is the logo of the XD Currency app" src="img/XD_currency_logo.png"></a>
-    <a href="register.php"><img class="profile" alt="this the icon of the profile page" src="img/XD_currency_icons_profile.png"></a>
-    <a href="transfer.php"><img class="transfer" alt="this the icon of the transfer page" src="img/XD_currency_icons_transfer.png"></a>
-    <a href="list.php"><img class="list" alt="this the icon of the list page" src="img/XD_currency_icons_list.png"></a>
-</nav>
+  
 <div class="container-account">
 <div class="container">
     <div class="form">
