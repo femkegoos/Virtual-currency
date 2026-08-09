3dba75637c4de0c2dc964122e0ce74f6bd2f9126
femkegoos <femke.goos.01@gmail.com>
2026-08-09 15:02:12 +0200

    refactor: remove list and profile icons from bottom navigation

--- nav.php
+++ nav.php
@@ -16,5 +16,4 @@
     <div class="nav-container">
     <a href="index.php"><img class="logo" alt="this is the logo of the XD Currency app" src="img/XD_currency_logo.png"></a>
     <a href="transfer.php"><img class="transfer" alt="this the icon of the transfer page" src="img/XD_currency_icons_transfer.png"></a>
-    <a href="list.php"><img class="list" alt="this the icon of the list page" src="img/XD_currency_icons_list.png"></a>
 </nav>
