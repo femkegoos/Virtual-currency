981d97e9fbdc6a4c82b3562f97d71d1d6d3209b5
femkegoos <femke.goos.01@gmail.com>
2026-08-09 16:49:57 +0200

    fix: title of the transactions into two sections

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['id'])) {
   <div class="container-account">
   <div class="container">
     <div class="all-transactions">
-    <h2 class="all-transactions-title">Welkom <br><?php echo htmlspecialchars($_SESSION['username']); ?> bij XD Currency!</h2>
+    <h2 class="all-transactions-title">Welkom <?php echo htmlspecialchars($_SESSION['username']); ?><br>bij XD Currency!</h2>
     <h1>Transacties</h1>
     <div class="transaction-list">
        <a href="transfer.php?id=1" class="transaction link"><p>Iemand heeft 10 XD naar jou overgemaakt</p>
