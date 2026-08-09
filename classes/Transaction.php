59e20606fcffe210ad9bf13888b15c0a4a38f4f3
femkegoos <femke.goos.01@gmail.com>
2026-08-09 17:54:47 +0200

    feat: add the getter and setters of the private elements

--- classes/Transaction.php
+++ classes/Transaction.php
@@ -9,4 +9,105 @@ class Transaction
     private $reason;
     private $date_created;
 
+    
+
+    /**
+     * Get the value of sender_id
+     */ 
+    public function getSender_id()
+    {
+        return $this->sender_id;
+    }
+
+    /**
+     * Set the value of sender_id
+     *
+     * @return  self
+     */ 
+    public function setSender_id($sender_id)
+    {
+        $this->sender_id = $sender_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of receiver_id
+     */ 
+    public function getReceiver_id()
+    {
+        return $this->receiver_id;
+    }
+
+    /**
+     * Set the value of receiver_id
+     *
+     * @return  self
+     */ 
+    public function setReceiver_id($receiver_id)
+    {
+        $this->receiver_id = $receiver_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of amount
+     */ 
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set the value of amount
+     *
+     * @return  self
+     */ 
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of reason
+     */ 
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Set the value of reason
+     *
+     * @return  self
+     */ 
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date_created
+     */ 
+    public function getDate_created()
+    {
+        return $this->date_created;
+    }
+
+    /**
+     * Set the value of date_created
+     *
+     * @return  self
+     */ 
+    public function setDate_created($date_created)
+    {
+        $this->date_created = $date_created;
+
+        return $this;
+    }
 }
