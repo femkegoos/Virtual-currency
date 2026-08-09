5df9d158507c3a1201f70c405ce3686c885f17c6
femkegoos <femke.goos.01@gmail.com>
2026-08-09 20:10:31 +0200

    fix: title of all the pages

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['id'])) {
   <div class="container-account">
   <div class="container">
     <div class="all-transactions">
-    <h2 class="all-transactions-title">Welkom <?php echo htmlspecialchars($_SESSION['username']); ?><br>bij XD Currency!</h2>
+    <h2>Welkom <?php echo htmlspecialchars($_SESSION['username']); ?><br>bij XD Currency!</h2>
     <h1>Jouw transacties</h1>
     <div class="transaction-list">
        <a href="transfer.php?id=1" class="transaction link"><p>Iemand heeft 10 XD naar jou overgemaakt</p>
