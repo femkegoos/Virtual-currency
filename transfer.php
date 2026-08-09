c621f21fc97bb62aa3cc88f640bcd429cbf4adb5
femkegoos <femke.goos.01@gmail.com>
2026-08-09 20:46:28 +0200

    fix: textarea input of the reason field

--- transfer.php
+++ transfer.php
@@ -40,7 +40,7 @@ if (!isset($_SESSION['id'])) {
         <label for="amount">Bedrag:</label><br>
         <input type="number" name="amount" id="amount" step="0.01" min="1" required><br>
         <label for="reason">Reden:</label><br>
-        <input type="textarea" name="reason" id="reason" required><br>
+        <textarea name="reason" id="reason" required></textarea><br>
 
         <input class="btn" type="submit" value="Verzenden">
         <?php if (isset($error)) {
