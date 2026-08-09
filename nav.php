0b05cfdfd6b61c707f032369898aad119fc99bf9
femkegoos <femke.goos.01@gmail.com>
2026-08-09 13:44:09 +0200

    feat: add username to the top header navigation

--- nav.php
+++ nav.php
@@ -4,7 +4,10 @@
 <header class="top-nav">
     <div class="top-nav-container">
    <p class="saldo"><?php echo htmlspecialchars($_SESSION['balance']); ?> XD</p>
+   <div class="user-info">
+    <p class="username"><?php echo htmlspecialchars($_SESSION['username']); ?></p>
    <a href="logout.php"><img class="logout" alt="this the icon of the logout page" src="img/XD_currency_icons_logout.png"></a>
+    </div>
     </div>
 </header>
   <?php endif; ?>
