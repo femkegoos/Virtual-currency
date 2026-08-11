deb4dbbe83dfb552e1a85b8935db86f2b1c1b865
femkegoos <femke.goos.01@gmail.com>
2026-08-11 17:58:34 +0200

    feat: content detail transaction to test

--- transaction.php
+++ transaction.php
@@ -27,7 +27,16 @@ $transactions = Transaction::getUserTransactions($_SESSION['id']);
   <div class="container-account">
   <div class="container">
     <div class="content">
-   
+    <h2>Transactie 20/08/2026</h2>
+    <div class="transaction">
+      <p><b>Van:</b>Femke</p>
+      <p><b>Naar</b>Silke</p>
+      <p><b>Bedrag:</b>10 XD</p>
+      <p><b>Reden:</b>Shopping terugbetaling</p>
+      <p><b>Datum:</b>Maandag 20 augustus 2026</p>
+    </div>
+    <a href="index.php" class="btn"> Terug naar alle transacties</a>
+    </div>
   </div>
   </div>
   
