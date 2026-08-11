deca53fdc7067c21e123f960762e4d809f5fc55f
femkegoos <femke.goos.01@gmail.com>
2026-08-11 17:20:02 +0200

    fix: transaction class function chronologic numbring

--- classes/Transaction.php
+++ classes/Transaction.php
@@ -159,7 +159,7 @@ class Transaction
         return true;
     }
 
-    public static function getUserTransactions($userId){
+    public static function getById($id, $userId){
          $conn = Db::getConnection();
         $statement = $conn->prepare("SELECT transactions.*, 
         sender.username as sender_username,
@@ -167,8 +167,9 @@ class Transaction
         FROM transactions
         JOIN users as sender ON transactions.sender_id = sender.id
         JOIN users as receiver ON transactions.receiver_id = receiver.id
-        WHERE transactions.sender_id = :userId or transactions.receiver_id = :userId
-        ORDER BY transactions.date_created DESC");
+        WHERE transactions.id = :id 
+        AND (transactions.sender_id = :userId OR transactions.receiver_id = :userId)");
+         $statement->bindValue(':id', $id);
          $statement->bindValue(':userId', $userId);
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_ASSOC);
