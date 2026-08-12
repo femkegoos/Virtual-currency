b21e5557014bcb0b59d4953ac97fdd3e2b87262c
femkegoos <femke.goos.01@gmail.com>
2026-08-12 12:06:43 +0200

    feat: add the ajax for searching the username

---
+++ ajax/searchUsers.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include_once(__DIR__ . '/classes/Db.php');
+include_once(__DIR__ . '/classes/User.php');
+
+if (!empty($_POST)){
+    if (isset($_POST['username']) && !empty($_POST['usernames'])){
+        $username = $_POST['username'];
+        $user = new User();
+        $items = $user->getByUsername($username);
+        $response = ['status' => 'succes', 'items' => $items];
+        header("Content-Type: application/json");
+        echo json_encode($response);
+    }
+}
+?>
