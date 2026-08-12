fc7dbaf678badaa1907cc6d51652879048ec4940
femkegoos <femke.goos.01@gmail.com>
2026-08-12 18:38:04 +0200

    fix: check for users to search if you are loged in

--- ajax/searchUsers.php
+++ ajax/searchUsers.php
@@ -3,6 +3,13 @@ session_start();
 include_once(__DIR__ . '/../classes/Db.php');
 include_once(__DIR__ . '/../classes/User.php');
 
+
+    if (!isset($_SESSION['id'])){
+        header("Content-Type: application/json");
+        echo json_encode(['status' => 'error', 'message' =>'Niet ingelogd']);
+        exit;
+    }
+
 if (!empty($_POST)){
     if (isset($_POST['username']) && !empty($_POST['username'])){
         $username = $_POST['username'];
