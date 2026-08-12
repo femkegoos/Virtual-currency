39cec50a639d3ebf27f86d48e4fd0a8215002be9
femkegoos <femke.goos.01@gmail.com>
2026-08-12 17:57:15 +0200

    feat: add Javascript to make the refreshing of the balance work

--- nav.php
+++ nav.php
@@ -3,13 +3,28 @@
 
 <header class="top-nav">
     <div class="top-nav-container">
-   <p class="saldo"><?php echo htmlspecialchars($_SESSION['balance']); ?> XD</p>
+   <p class="saldo" id="saldo"><?php echo htmlspecialchars($_SESSION['balance']); ?> XD</p>
    <div class="user-info">
     <p class="username"><?php echo htmlspecialchars($_SESSION['username']); ?></p>
    <a href="logout.php"><img class="logout" alt="this the icon of the logout page" src="img/XD_currency_icons_logout.png"></a>
     </div>
     </div>
 </header>
+<script>
+  const saldoElement = document.getElementById("saldo");
+  function updateBalance() {
+    fetch("ajax/updateBalance.php")
+    .then(response => response.json())
+    .then(data => {
+      if(data.status ==="succes"){
+        saldoElement.textContent = data.balance + "XD";
+      }
+    })
+    .catch(error => console.error("Error:", error));
+  }
+  setInterval(updateBalance, 10000);
+</script>
+
   <?php endif; ?>
   
   <nav class="bottom-nav">
