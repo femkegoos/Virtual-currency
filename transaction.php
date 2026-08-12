da7e077d1f576140ce08fc941a065c755f5741dd
femkegoos <femke.goos.01@gmail.com>
2026-08-12 10:09:11 +0200

    fix: little mistakes in the transaction file

--- transaction.php
+++ transaction.php
@@ -8,7 +8,7 @@ include_once(__DIR__ . '/classes/Db.php');
 include_once(__DIR__ . '/classes/User.php');
 include_once(__DIR__ . '/classes/Transaction.php');
 
-if (isset($_GET['id']) && !empty($_GET['id'])){
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $transaction = Transaction::getById($_GET['id'], $_SESSION['id']);
 }
 
@@ -36,9 +36,16 @@ $maanden = [
     'November' => 'november',
     'December' => 'december'
 ];
-$datum = date('l d F Y H:i', strtotime($transaction['date_created']));
-$datum = str_replace(array_keys($dagen), array_values($dagen), $datum);
-$datum = str_replace(array_keys($maanden), array_values($maanden), $datum);
+
+if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $transaction = Transaction::getById($_GET['id'], $_SESSION['id']);
+    if ($transaction) {
+        $datum = date('l d F Y H:i', strtotime($transaction['date_created']));
+        $datum = str_replace(array_keys($dagen), array_values($dagen), $datum);
+        $datum = str_replace(array_keys($maanden), array_values($maanden), $datum);
+    }
+}
+
 
 
 
@@ -46,30 +53,33 @@ $datum = str_replace(array_keys($maanden), array_values($maanden), $datum);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://use.typekit.net/tkk7yuy.css">
+    <link rel="stylesheet" href="https://use.typekit.net/tkk7yuy.css">
     <title>Transactie</title>
-</head> 
+</head>
+
 <body>
-     <?php include_once(__DIR__ . '/nav.php'); ?>
-  <div class="container-account">
-  <div class="container">
-    <div class="content">
-   <h2>Transactie <?php echo date('d/m/Y', strtotime($transaction['date_created'])); ?></h2>
-    <div class="transaction">
-      <p><b>Van: </b><?php echo htmlspecialchars($transaction['sender_username']);?></p>
-      <p><b>Naar: </b><?php echo htmlspecialchars($transaction['receiver_username']);?></p>
-      <p><b>Bedrag: </b><?php echo htmlspecialchars($transaction['amount']);?> XD</p>
-      <p><b>Reden: </b><?php echo htmlspecialchars($transaction['reason']);?></p>
-      <p><b>Datum: </b><?php echo $datum; ?></p><br><br>
+    <?php include_once(__DIR__ . '/nav.php'); ?>
+    <div class="container-account">
+        <div class="container">
+            <div class="content">
+                <h2>Transactie <?php echo date('d/m/Y', strtotime($transaction['date_created'])); ?></h2>
+                <div class="transaction">
+                    <p><b>Van: </b><?php echo htmlspecialchars($transaction['sender_username']); ?></p>
+                    <p><b>Naar: </b><?php echo htmlspecialchars($transaction['receiver_username']); ?></p>
+                    <p><b>Bedrag: </b><?php echo htmlspecialchars($transaction['amount']); ?> XD</p>
+                    <p><b>Reden: </b><?php echo htmlspecialchars($transaction['reason']); ?></p>
+                    <p><b>Datum: </b><?php echo $datum; ?></p><br><br>
+                </div>
+                <a href="index.php" class="btn"> Terug naar alle transacties</a>
+            </div>
+        </div>
     </div>
-    <a href="index.php" class="btn"> Terug naar alle transacties</a>
-    </div>
-  </div>
-  </div>
-  
+
 </body>
+
 </html>
