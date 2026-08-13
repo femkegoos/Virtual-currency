ecdd908ffad0f1c27ebd0238175728cc7013005b
femkegoos <femke.goos.01@gmail.com>
2026-08-13 09:57:45 +0200

    fix: add extra code to the transaction ajax to get the response

--- ajax/getTransactions.php
+++ ajax/getTransactions.php
@@ -5,15 +5,15 @@ include_once(__DIR__ . '/../classes/User.php');
 include_once(__DIR__ . '/../classes/Transaction.php');
 
 
-    if (!isset($_SESSION['id'])){
-        header("Content-Type: application/json");
-        echo json_encode(['status' => 'error', 'message' =>'Niet ingelogd']);
-        exit;
-    }
+if (!isset($_SESSION['id'])) {
+    header("Content-Type: application/json");
+    echo json_encode(['status' => 'error', 'message' => 'Niet ingelogd']);
+    exit;
+}
 
-    $transactions = Transaction::getUserTransactions($_SESSION['id']);
+$transactions = Transaction::getUserTransactions($_SESSION['id']);
 
-    $dagen = [
+$dagen = [
     'Monday' => 'maandag',
     'Tuesday' => 'dinsdag',
     'Wednesday' => 'woensdag',
@@ -38,5 +38,24 @@ $maanden = [
     'December' => 'december'
 ];
 
+$response = [];
 
+foreach ($transactions as $transaction) {
+    $datum = date('l d F Y', strtotime($transaction['date_created']));
+    $datum = str_replace(array_keys($dagen), array_values($dagen), $datum);
+    $datum = str_replace(array_keys($maanden), array_values($maanden), $datum);
+
+    $isSender = ($transaction['sender_id'] == $_SESSION['id']);
+
+    $response[] = [
+        'id' => $transaction['id'],
+        'amount' => htmlspecialchars($transaction['amount']),
+        'datum' => $datum,
+        'isSender' => $isSender,
+        'otherUsername' => htmlspecialchars($isSender ? $transaction['receiver_username'] : $transaction['sender_username']),
+        'ownerUsername' => htmlspecialchars($_SESSION['username'])
+    ];
+}
+header("Content-Type: application/json");
+echo json_encode($response);
 ?>
