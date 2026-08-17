92ca11107a574df40b11bd86f8a7479231efe6ed
femkegoos <femke.goos.01@gmail.com>
2026-08-17 14:38:04 +0200

    fix: the class include on the getTransaction ajax file

--- ajax/getTransactions.php
+++ ajax/getTransactions.php
@@ -3,7 +3,7 @@ session_start();
 include_once(__DIR__ . '/../classes/Db.php');
 include_once(__DIR__ . '/../classes/User.php');
 include_once(__DIR__ . '/../classes/Transaction.php');
-
+include_once(__DIR__ . '/../classes/Date.php');
 
 if (!isset($_SESSION['id'])) {
     header("Content-Type: application/json");
@@ -13,37 +13,11 @@ if (!isset($_SESSION['id'])) {
 
 $transactions = Transaction::getUserTransactions($_SESSION['id']);
 
-$dagen = [
-    'Monday' => 'maandag',
-    'Tuesday' => 'dinsdag',
-    'Wednesday' => 'woensdag',
-    'Thursday' => 'donderdag',
-    'Friday' => 'vrijdag',
-    'Saturday' => 'zaterdag',
-    'Sunday' => 'zondag'
-];
-
-$maanden = [
-    'January' => 'januari',
-    'February' => 'februari',
-    'March' => 'maart',
-    'April' => 'april',
-    'May' => 'mei',
-    'June' => 'juni',
-    'July' => 'juli',
-    'August' => 'augustus',
-    'September' => 'september',
-    'October' => 'oktober',
-    'November' => 'november',
-    'December' => 'december'
-];
 
 $response = [];
 
 foreach ($transactions as $transaction) {
-    $datum = date('l d F Y', strtotime($transaction['date_created']));
-    $datum = str_replace(array_keys($dagen), array_values($dagen), $datum);
-    $datum = str_replace(array_keys($maanden), array_values($maanden), $datum);
+   $datum = Date::format($transaction['date_created']);
 
     $isSender = ($transaction['sender_id'] == $_SESSION['id']);
 
